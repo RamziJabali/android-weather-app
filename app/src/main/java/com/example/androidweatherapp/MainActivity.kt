55aa4bd71971e28ba83ce9d154d119b6d6a03c75
RamziJabali <ramzieljabali@gmail.com>
2021-06-29 19:57:04 -0700

    Added Progress Bar while API is being parsed, Added Methods remove and show Views

--- app/src/main/java/com/example/androidweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/androidweatherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import com.example.androidweatherapp.models.Location
 import com.example.androidweatherapp.models.WeatherForLocation
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         const val API_BASE_URL = "https://www.metaweather.com/"
     }
 
+    var loadingDialog = LoadingDialog(this)
+
     lateinit var api: JsonWeatherApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +26,19 @@ class MainActivity : AppCompatActivity() {
             .withApiBaseUrl(API_BASE_URL)
             .build()
 
-        var loadingDialog = LoadingDialog(this)
 
         getCountryAndCityName(2487956)
-//        getCities()
     }
 
-    //44418
     private fun getCountryAndCityName(whereOnEarthId: Int) {
+        stopShowingViewsWhileLoading()
+        loadingDialog.startLoadingDialog()
         val call = api.getWeatherForWhereOnEarthId(whereOnEarthId)
         call.enqueue(
             object : Callback<WeatherForLocation> {
                 override fun onFailure(call: Call<WeatherForLocation>, t: Throwable) {
+                    loadingDialog.dismissDialog()
+                    showViews()
                     findViewById<TextView>(R.id.city).text = t.message
                     findViewById<TextView>(R.id.state).text = t.message
                     findViewById<TextView>(R.id.temperature).text = t.message
@@ -44,6 +48,8 @@ class MainActivity : AppCompatActivity() {
                     call: Call<WeatherForLocation>,
                     response: Response<WeatherForLocation>
                 ) {
+                    loadingDialog.dismissDialog()
+                    showViews()
                     if (!response.isSuccessful) {
                         findViewById<TextView>(R.id.city).text = "Code: " + response.code()
                         findViewById<TextView>(R.id.state).text = "Code: " + response.code()
@@ -52,35 +58,22 @@ class MainActivity : AppCompatActivity() {
                     val weatherForLocation = response.body()
                     findViewById<TextView>(R.id.city).text = weatherForLocation!!.cityTitle
                     findViewById<TextView>(R.id.state).text = weatherForLocation.parentRegion.title
-                    findViewById<TextView>(R.id.temperature).text = weatherForLocation.consolidatedWeather[0].theTemp.toString() + "C"
-                    findViewById<TextView>(R.id.pressureValueText).text = weatherForLocation.consolidatedWeather[0].airPressure.toString()
-                    findViewById<TextView>(R.id.humidityValueText).text = weatherForLocation.consolidatedWeather[0].humidity.toString()
-                    findViewById<TextView>(R.id.windSpeedValueText).text = weatherForLocation.consolidatedWeather[0].windSpeed.toString()
+                    findViewById<TextView>(R.id.temperature).text =
+                        weatherForLocation.consolidatedWeather[0].theTemp.toString() + "C"
+                    findViewById<TextView>(R.id.pressureValueText).text =
+                        weatherForLocation.consolidatedWeather[0].airPressure.toString()
+                    findViewById<TextView>(R.id.humidityValueText).text =
+                        weatherForLocation.consolidatedWeather[0].humidity.toString()
+                    findViewById<TextView>(R.id.windSpeedValueText).text =
+                        weatherForLocation.consolidatedWeather[0].windSpeed.toString()
                 }
             })
     }
 
-    //San%20Francisco, 2487956
     private fun getCities() {
         val call = api.getCitiesForQuery("london") //error here
         call.enqueue(
             object : Callback<List<Location>> {
-//                override fun onFailure(call: Call<Location>, t: Throwable) {
-//                    findViewById<TextView>(R.id.city).text = t.message
-//                    findViewById<TextView>(R.id.state).text = t.message
-//                }
-//
-//                override fun onResponse(call: Call<Location>, response: Response<Location>) {
-//                    if (!response.isSuccessful) {
-//                        findViewById<TextView>(R.id.city).text = "Code: " + response.code()
-//                        findViewById<TextView>(R.id.state).text = "Code: " + response.code()
-//                        return
-//                    }
-//                    val location = response.body()
-//                    findViewById<TextView>(R.id.city).text = location!!.cityTitle
-//                    findViewById<TextView>(R.id.state).text = location.location_type
-//                }
-
                 override fun onFailure(call: Call<List<Location>>, t: Throwable) {
                     findViewById<TextView>(R.id.city).text = t.message
                     findViewById<TextView>(R.id.state).text = t.message
@@ -97,11 +90,34 @@ class MainActivity : AppCompatActivity() {
                         return
                     }
                     val locationOfCity = response.body()
-                    findViewById<TextView>(R.id.city).text =  locationOfCity!![0].cityTitle
+                    findViewById<TextView>(R.id.city).text = locationOfCity!![0].cityTitle
                     findViewById<TextView>(R.id.state).text = locationOfCity[0].location_type
                 }
 
             })
+    }
+
+    private fun stopShowingViewsWhileLoading() {
+        findViewById<TextView>(R.id.city).visibility = View.GONE
+        findViewById<TextView>(R.id.state).visibility = View.GONE
+        findViewById<TextView>(R.id.temperature).visibility = View.GONE
+        findViewById<TextView>(R.id.pressureValueText).visibility = View.GONE
+        findViewById<TextView>(R.id.humidityValueText).visibility = View.GONE
+        findViewById<TextView>(R.id.windSpeedValueText).visibility = View.GONE
+        findViewById<TextView>(R.id.pressureText).visibility = View.GONE
+        findViewById<TextView>(R.id.humidityText).visibility = View.GONE
+        findViewById<TextView>(R.id.windSpeedText).visibility = View.GONE
+    }
+    private fun showViews() {
+        findViewById<TextView>(R.id.city).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.state).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.temperature).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.pressureValueText).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.humidityValueText).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.windSpeedValueText).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.pressureText).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.humidityText).visibility = View.VISIBLE
+        findViewById<TextView>(R.id.windSpeedText).visibility = View.VISIBLE
     }
 }
 
