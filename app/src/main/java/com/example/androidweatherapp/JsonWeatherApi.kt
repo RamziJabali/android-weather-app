7c632b0523ee5c8992a06620e432374c840511df
RamziJabali <ramzieljabali@gmail.com>
2021-06-29 17:59:38 -0700

    Made changes to the location end point manager and updated code in the main

--- app/src/main/java/com/example/androidweatherapp/JsonWeatherApi.kt
+++ app/src/main/java/com/example/androidweatherapp/JsonWeatherApi.kt
@@ -5,14 +5,15 @@ import com.example.androidweatherapp.models.WeatherForLocation
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface JsonWeatherApi {
 
     @GET("/api/location/{whereOnEarthId}")
     fun getWeatherForWhereOnEarthId(@Path("whereOnEarthId") whereOnEarthId: Int): Call<WeatherForLocation>
 
-    @GET("/api/location/search/?query={cityName}")
-    fun getCitiesForQuery(@Path("cityName") cityName: String): Call<Location>
+    @GET("/api/location/search")
+    fun getCitiesForQuery(@Query("query") cityName: String): Call<List<Location>>
 
 
 }
--- app/src/main/java/com/example/androidweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/androidweatherapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.androidweatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
+import com.example.androidweatherapp.models.Location
 import com.example.androidweatherapp.models.WeatherForLocation
 import retrofit2.Call
 import retrofit2.Callback
@@ -14,39 +14,93 @@ class MainActivity : AppCompatActivity() {
         const val API_BASE_URL = "https://www.metaweather.com/"
     }
 
+    lateinit var api: JsonWeatherApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        val api = ApiServiceBuilder(JsonWeatherApi::class.java)
+        api = ApiServiceBuilder(JsonWeatherApi::class.java)
             .withApiBaseUrl(API_BASE_URL)
             .build()
 
-        val call = api.getWeatherForWhereOnEarthId(44418)
+        getCountryAndCityName(2487956)
+//        getCities()
+    }
 
-
-
-        call.enqueue(object : Callback<WeatherForLocation> {
-            override fun onFailure(call: Call<WeatherForLocation>, t: Throwable) {
-                findViewById<TextView>(R.id.city).text = t.message
-                findViewById<TextView>(R.id.state).text = t.message
-            }
-
-            override fun onResponse(
-                call: Call<WeatherForLocation>,
-                response: Response<WeatherForLocation>
-            ) {
-                if (!response.isSuccessful) {
-                    findViewById<TextView>(R.id.city).text = "Code: " + response.code()
-                    findViewById<TextView>(R.id.state).text = "Code: " + response.code()
-                    return
+    //44418
+    private fun getCountryAndCityName(whereOnEarthId: Int) {
+        val call = api.getWeatherForWhereOnEarthId(whereOnEarthId)
+        call.enqueue(
+            object : Callback<WeatherForLocation> {
+                override fun onFailure(call: Call<WeatherForLocation>, t: Throwable) {
+                    findViewById<TextView>(R.id.city).text = t.message
+                    findViewById<TextView>(R.id.state).text = t.message
+                    findViewById<TextView>(R.id.temperature).text = t.message
                 }
-                val weatherForLocation = response.body()
-                findViewById<TextView>(R.id.city).text = weatherForLocation!!.cityTitle
-                findViewById<TextView>(R.id.state).text = weatherForLocation.parentRegion.title
-            }
-        })
+
+                override fun onResponse(
+                    call: Call<WeatherForLocation>,
+                    response: Response<WeatherForLocation>
+                ) {
+                    if (!response.isSuccessful) {
+                        findViewById<TextView>(R.id.city).text = "Code: " + response.code()
+                        findViewById<TextView>(R.id.state).text = "Code: " + response.code()
+                        return
+                    }
+                    val weatherForLocation = response.body()
+                    findViewById<TextView>(R.id.city).text = weatherForLocation!!.cityTitle
+                    findViewById<TextView>(R.id.state).text = weatherForLocation.parentRegion.title
+                    findViewById<TextView>(R.id.temperature).text = weatherForLocation.consolidatedWeather[0].theTemp.toString() + "C"
+                    findViewById<TextView>(R.id.pressureValueText).text = weatherForLocation.consolidatedWeather[0].airPressure.toString()
+                    findViewById<TextView>(R.id.humidityValueText).text = weatherForLocation.consolidatedWeather[0].humidity.toString()
+                    findViewById<TextView>(R.id.windSpeedValueText).text = weatherForLocation.consolidatedWeather[0].windSpeed.toString()
+                }
+            })
+    }
+
+    //San%20Francisco, 2487956
+    private fun getCities() {
+        val call = api.getCitiesForQuery("london") //error here
+        call.enqueue(
+            object : Callback<List<Location>> {
+//                override fun onFailure(call: Call<Location>, t: Throwable) {
+//                    findViewById<TextView>(R.id.city).text = t.message
+//                    findViewById<TextView>(R.id.state).text = t.message
+//                }
+//
+//                override fun onResponse(call: Call<Location>, response: Response<Location>) {
+//                    if (!response.isSuccessful) {
+//                        findViewById<TextView>(R.id.city).text = "Code: " + response.code()
+//                        findViewById<TextView>(R.id.state).text = "Code: " + response.code()
+//                        return
+//                    }
+//                    val location = response.body()
+//                    findViewById<TextView>(R.id.city).text = location!!.cityTitle
+//                    findViewById<TextView>(R.id.state).text = location.location_type
+//                }
+
+                override fun onFailure(call: Call<List<Location>>, t: Throwable) {
+                    findViewById<TextView>(R.id.city).text = t.message
+                    findViewById<TextView>(R.id.state).text = t.message
+                    findViewById<TextView>(R.id.temperature).text = t.message
+                }
+
+                override fun onResponse(
+                    call: Call<List<Location>>,
+                    response: Response<List<Location>>
+                ) {
+                    if (!response.isSuccessful) {
+                        findViewById<TextView>(R.id.city).text = "Code: " + response.code()
+                        findViewById<TextView>(R.id.state).text = "Code: " + response.code()
+                        return
+                    }
+                    val locationOfCity = response.body()
+                    findViewById<TextView>(R.id.city).text =  locationOfCity!![0].cityTitle
+                    findViewById<TextView>(R.id.state).text = locationOfCity[0].location_type
+                }
+
+            })
     }
 }
 
